ec15dd3559e42401daac653b2b54497d4ee57f82
Ivan Shvedunov <ivan4th@gmail.com>
2015-03-20 07:35:31 +0300

    Restored summary window.

--- public/main.js
+++ public/main.js
@@ -10,21 +10,27 @@ angular.module("ctelemetryApp", ["ngRoute", "ui.bootstrap", "ngSanitize"])
   .config(function ($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-      .when("/ct/", {
+      .when("/ct/main/", {
         templateUrl: "views/main.html",
         controller: "MainCtrl"
       })
-      .when("/ct/:rest*", {
+      .when("/ct/main/:rest*", {
         templateUrl: "views/main.html",
         controller: "MainCtrl"
+      })
+      .when("/ct/log/:rest*", {
+        templateUrl: "views/log.html",
+        controller: "LogCtrl"
       })
       .otherwise({
-        redirectTo: "/ct/"
+        redirectTo: "/ct/main/"
       });
   })
+
   .run(function () {//(ControlClient) {
     // ControlClient.startup();
   })
+
   .factory("getDate", function () {
     return function getDate (ts) {
       var d = new Date();
@@ -32,20 +38,81 @@ angular.module("ctelemetryApp", ["ngRoute", "ui.bootstrap", "ngSanitize"])
       return d;
     };
   })
+
   .filter("ctfloat", function ($filter) {
     return function (v, fractionSize) {
       return typeof v == "number" ?
         $filter("number")(v, fractionSize == null ? 4 : fractionSize).replace(",", "") : v;
     };
   })
-  .controller("NavBarCtrl", function ($scope, $http, $routeParams) {
+
+  .factory("ValueUpdater", function ($rootScope, getDate) {
+    // TBD: instead of using valueMap, just broadcast the event
+    var valueMap = {}, ws = null;
+
+    function ensureActive() {
+      if (ws && (ws.readyState == WebSocket.CONNECTING || ws.readyState == WebSocket.OPEN))
+        return;
+      // FIXME: use angular-websocket
+      var ws = new window.WebSocket("ws://" + document.location.host + "/ws-data");
+      ws.onopen = function () {
+        console.log("ws: open");
+      };
+      ws.onmessage = function (message) {
+        var d = JSON.parse(message.data);
+        // console.log("ws: %o", JSON.stringify(d));
+        var keyPrefix = d.topic + "|||";
+        d.cells.forEach(function (cellItem) {
+          var key = keyPrefix + cellItem[0],
+              value = cellItem[1];
+          if (valueMap.hasOwnProperty(key)) {
+            // console.log("key found: " + key + "; val=" + value);
+            var r = valueMap[key];
+            r.count++; // FIXME: that's not quite correct either
+            r.value = value;
+            r.timestamp = getDate(d.ts);
+          }
+        });
+        $rootScope.$digest();
+      };
+      ws.onclose = function () {
+        console.log("ws close");
+      };
+      // var ws = $websocket("/ws-data");
+      // ws.onMessage(function (message) {
+      //   console.log("ws: %o", message);
+      // });
+    }
+
+    return {
+      ensureActive: ensureActive,
+      setValueMap: function (newValueMap) {
+        valueMap = newValueMap;
+        ensureActive();
+      }
+    };
+  })
+
+  .controller("NavBarCtrl", function ($scope, $http, $routeParams, $location) {
+    $scope.sections = [];
+    $scope.active = function (section) {
+      return section.topicPrefix == ($routeParams.rest ? "/" + $routeParams.rest : "/");
+    };
+    $scope.current = function () {
+      for (var i = 0; i < $scope.sections.length; ++i) {
+        var sec = $scope.sections[i];
+        if ($scope.active(sec))
+          return sec;
+      }
+      return null;
+    };
+    $scope.showingLog = function () {
+      return /\/ct\/log\//.test($location.path()); // FIXME?
+    };
     $http({
       url: "/sections",
       method: "GET"
     }).success(function (result) {
-      $scope.active = function (section) {
-        return section.topicPrefix == ($routeParams.rest || "/");
-      };
       $scope.sections = result.sections.map(function (section) {
         return {
           topicPrefix: section[0],
@@ -55,10 +122,36 @@ angular.module("ctelemetryApp", ["ngRoute", "ui.bootstrap", "ngSanitize"])
       console.log("sections: %o", $scope.sections);
     });
   })
-  .controller("MainCtrl", function ($scope, $http, $routeParams, $modal, getDate) {
+
+  .controller("MainCtrl", function ($scope, $http, $routeParams, getDate, ValueUpdater) {
     var subtopic = $routeParams.rest ? "/" + $routeParams.rest : "";
     // $scope.loaded = false;
     $scope.data = [];
+    $http({
+      url: "/latest" + subtopic,
+      method: "GET"
+    }).success(function (result) {
+      // $scope.loaded = true;
+      var valueMap = {};
+      $scope.data = result.cells.map(function (item) {
+        var r = {
+          topic: item[0],
+          topicDisplayName: item[1],
+          cell: item[2],
+          cellDisplayName: item[3],
+          count: item[4],
+          timestamp: getDate(item[5]),
+          value: item[6]
+        };
+        valueMap[r.topic + "|||" + r.cell] = r;
+        return r;
+      });
+      ValueUpdater.setValueMap(valueMap);
+    });
+  })
+
+  .controller("LogCtrl", function ($scope, $http, $routeParams, $modal, getDate) {
+    var subtopic = $routeParams.rest ? "/" + $routeParams.rest : ""; // FIXME (dup)
     $scope.topicMap = {};
     $scope.topics = null;
     $scope.activeTopics = function () {
@@ -70,7 +163,6 @@ angular.module("ctelemetryApp", ["ngRoute", "ui.bootstrap", "ngSanitize"])
         return t.id;
       }).sort().join(",");
     };
-    console.log("main ctrl init");
     $scope.loadLog = function () {
       var activeTopics = $scope.activeTopics();
       var filter = activeTopics === null ? "" : "?filter=" + activeTopics;
@@ -124,56 +216,8 @@ angular.module("ctelemetryApp", ["ngRoute", "ui.bootstrap", "ngSanitize"])
       $scope.loadLog();
     });
     $scope.loadLog();
-
-    $http({
-      url: "/latest" + subtopic,
-      method: "GET"
-    }).success(function (result) {
-      // $scope.loaded = true;
-      var valueMap = {};
-      $scope.data = result.cells.map(function (item) {
-        var r = {
-          topic: item[0],
-          topicDisplayName: item[1],
-          cell: item[2],
-          cellDisplayName: item[3],
-          count: item[4],
-          timestamp: getDate(item[5]),
-          value: item[6]
-        };
-        valueMap[r.topic + "|||" + r.cell] = r;
-        return r;
-      });
-      var ws = new window.WebSocket("ws://" + document.location.host + "/ws-data");
-      ws.onopen = function () {
-        console.log("ws: open");
-      };
-      ws.onmessage = function (message) {
-        var d = JSON.parse(message.data);
-        // console.log("ws: %o", JSON.stringify(d));
-        var keyPrefix = d.topic + "|||";
-        d.cells.forEach(function (cellItem) {
-          var key = keyPrefix + cellItem[0],
-              value = cellItem[1];
-          if (valueMap.hasOwnProperty(key)) {
-            // console.log("key found: " + key + "; val=" + value);
-            var r = valueMap[key];
-            r.count++; // FIXME: that's not quite correct either
-            r.value = value;
-            r.timestamp = getDate(d.ts);
-          }
-        });
-        $scope.$digest();
-      };
-      ws.onclose = function () {
-        console.log("ws close");
-      };
-      // var ws = $websocket("/ws-data");
-      // ws.onMessage(function (message) {
-      //   console.log("ws: %o", message);
-      // });
-    });
   })
+
   .controller("EventWindowCtrl", function ($scope, $http, eventId, getDate) {
     $http({
       url: "/event/" + eventId,
@@ -185,4 +229,7 @@ angular.module("ctelemetryApp", ["ngRoute", "ui.bootstrap", "ngSanitize"])
     });
   });
 
+// TBD: display some kind of banner on the default page (or section list?)
+// TBD: custom filter instead of getDate
+// TBD: loading indicator, don't show 'empty' banners when the data aren't loaded yet
 // TBD: CSRF (XSRF) protection
